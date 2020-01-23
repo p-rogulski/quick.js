67a68b9c5ec2173b34c2e34d99f6ab1c95dff256
rogulskip <piotr.rgl@gmail.com>
2020-01-23 21:35:50 +0100

    struct.js - list, action remove

--- demo.js
+++ demo.js
@@ -1,13 +1,20 @@
 function demoList(){
     const struct = qk.struct;
     const list = struct.list();
-    list.add(1);
-    list.add(2);
-    list.add(3);
-    list.add(4);
-    list.add(5);
-    list.print((nodeValue)=>{
-        console.log(nodeValue);
+    list.add(10);
+    list.add(20);
+    list.add(30);
+    list.add(40);
+    list.add(50);
+
+    console.log("List state before remove");
+    list.print((nodeValue,index)=>{
+        console.log(`index:${index}, value:${nodeValue}`);
+    });
+    console.log("List state after removing element with index==2");
+    list.remove(2);
+    list.print((nodeValue,index)=>{
+        console.log(`index:${index}, value:${nodeValue}`);
     });
 }
 
