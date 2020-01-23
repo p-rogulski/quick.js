940e25c8df678622b1e3e59cfe106c4c9bcffdd7
rogulskip <piotr.rgl@gmail.com>
2020-01-23 21:35:50 +0100

    struct.js - list action remove

--- core/struct.js
+++ core/struct.js
@@ -7,7 +7,7 @@ const struct = (function () {
         listApi.add = function (value) {
             if (!head) {
                 head = {
-                    id: 1,
+                    id: 0,
                     value: value,
                 };
 
@@ -22,27 +22,58 @@ const struct = (function () {
             }
         }
 
+        listApi.remove = function (nodeId) {
+            let ptr, ptrPrev;
+            function _removeNode() {
+                if (ptr === head) {
+                    head = ptr.next;
+                } else if (ptr === tail) {
+                    delete ptrPrev.next;
+                    tail = ptrPrev;
+                } else if (ptrPrev) {
+                    ptrPrev.next = ptr.next;
+                    delete ptr.next;
+                }
 
-        listApi.print = function (callback) {
-            function process(currentNode) {
-                callback(currentNode.value);
-                if (currentNode.id !== tail.id) {
-                    process(currentNode.next);
+            }
+
+            function _process(currentNode) {
+                ptr = currentNode;
+                if (ptr.id === nodeId) {
+                    _removeNode();
+                } else if (ptr.id !== tail.id) {
+                    ptrPrev = ptr;
+                    ptr = ptr.next;
+                    _process(ptr);
+                } else {
+                    throw Error(`Element with index '${nodeId}' does not exsists.`);
                 }
             }
 
-
             if (head) {
-                process(head);
+                _process(head);
             }
         }
+
+        listApi.print = function (callback) {
+            function _process(currentNode) {
+                callback(currentNode.value, currentNode.id);
+                if (currentNode.id !== tail.id) {
+                    _process(currentNode.next);
+                }
+            }
+            if (head) {
+                _process(head);
+            }
+        }
+
 
         return listApi;
 
     }
 
     structApi.tree = function () {
-        
+
     }
 
     structApi.graph = function () {
@@ -55,4 +86,4 @@ const struct = (function () {
     }
 
     return structApi;
-})();;
+})();
